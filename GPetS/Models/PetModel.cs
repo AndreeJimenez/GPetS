d02caa6d0d60df2fe2460b806c45b06877e27330
Andree Jimenez <andree_jimenez@outlook.com>
2020-05-14 00:52:41 -0500

    Add NuGet Packages

---
+++ GPetS/Models/PetModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GPetS.Models
+{
+    public class PetModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Image64 { get; set; }
+        public DateTime PetDate { get; set; }
+        public string Gender { get; set; }
+        public string Race { get; set; }
+        public int Weight { get; set; }
+        public string Comments { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
